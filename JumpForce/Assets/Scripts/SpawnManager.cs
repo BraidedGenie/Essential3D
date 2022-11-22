227887342ebce71d6b1f2b1d0a6bea47e5883773
Jaimee Beard <jebeard@student.fullsail.edu>
2022-11-22 10:54:05 -0500

    added JumpForce project

---
+++ JumpForce/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private float startDelay = 2;
+    private float repateRate = 2;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repateRate);
+    }
+
+    void SpawnObstacle()
+    {
+        if (playerController.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
